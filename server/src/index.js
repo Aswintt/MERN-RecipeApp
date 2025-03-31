8f3b8e425fb097a23994e6373443bf63c4e53444
Aswin T <98648665+Aswintt@users.noreply.github.com>
2025-04-01 03:27:11 +0530

    Modified 2 render

--- server/src/index.js
+++ server/src/index.js
@@ -1,22 +1,23 @@
 import express from "express";
-import cors from 'cors';
-import mongoose from 'mongoose';
+import cors from "cors";
+import mongoose from "mongoose";
 
 import { userRouter } from "./routes/users.js";
 import { recipesRouter } from "./routes/recipes.js";
 
 const app = express();
-
+const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cors());
 
-app.use("/auth" , userRouter);
-app.use("/recipes" , recipesRouter);
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
 
-mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('Error connecting to MongoDB:', err));
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 
-app.listen(3001, ()=>{
-    console.log("Server STARTED...!!!")
+app.listen(port, () => {
+  console.log("Server STARTED... on !!! Port : ", port);
 });
